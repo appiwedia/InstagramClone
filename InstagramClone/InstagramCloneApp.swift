97f6c971cfe7af4f30c2aa947b4df0436aafefb8
Mickael MAS <mickael.mas@appiwedia.com>
2020-07-16 10:57:55 +0200

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Mickael Mas on 16/07/2020.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
